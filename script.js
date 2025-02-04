5461433bb6b00a15baf492f750bf498bcc4fd576
fmpanta <francisco.panta.23@gmail.com>
2025-02-04 19:54:28 +0100

    old

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
             // Track play count
             let songName = button.textContent.trim();
             let count = localStorage.getItem(songName) || 0;
-            count += 1;
+            count = parseInt(count) + 1;
             localStorage.setItem(songName, count);
 
             // Update the displayed counter
